ba87305150f70d80bbfd3210631b57387de6e5c8
KomaMix <ksabirov448@gmail.com>
2024-09-06 21:28:18 +0300

    Создал и применил миграцию базы данных

---
+++ CSV_Converter/Migrations/20240906182719_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CSV_Converter.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Region = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    ItemType = table.Column<string>(type: "text", nullable: false),
+                    SalesChannel = table.Column<string>(type: "text", nullable: false),
+                    OrderPriority = table.Column<string>(type: "text", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OrderID = table.Column<int>(type: "integer", nullable: false),
+                    ShipDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UnitsSold = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    UnitCost = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalRevenue = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalCost = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalProfit = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- CSV_Converter/Program.cs
+++ CSV_Converter/Program.cs
@@ -1,7 +1,15 @@
+using CSV_Converter.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
